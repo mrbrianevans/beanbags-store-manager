9f9369b34669c5834aba2c7805371c9104f01c15
Brian Evans <53117772+mrbrianevans@users.noreply.github.com>
2020-03-11 14:17:14 +0000

    Changed imports to single module (removed wildcard)
    Signed-off-by: Brian Evans <ebrian101@gmail.com>

--- OOP coursework/src/beanbags/Store.java
+++ OOP coursework/src/beanbags/Store.java
@@ -1,6 +1,11 @@
 package beanbags;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 /**
  * Store interface. This class provides a fully functioning implementor
